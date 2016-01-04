222638e83b750695d120d9e1b2d85a4be78675f7
Tyler Nagamine <Tyler Nagamine>
2016-01-04 04:16:08 -0800

    Use form in all views, back to working state. + Extra, page 209 testinggoat

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,22 +4,20 @@ from unittest import skip
 class ItemValidationTest(FunctionalTest):
 	def test_cannot_add_empty_list_items(self):
 		self.browser.get(self.server_url)
-		self.get_item_input_box(self).send_keys('\n')
+		self.get_item_input_box().send_keys('\n')
 
 		error = self.browser.find_element_by_css_selector('.has-error')
 		self.assertEqual(error.text, "You can't have an empty list item")
 
-		self.get_item_input_box(self).send_keys('Buy milk\n')
+		self.get_item_input_box().send_keys('Buy milk\n')
 		self.check_for_row_in_list_table('1: Buy milk')
 
-		self.get_item_input_box(self).send_keys('\n')
+		self.get_item_input_box().send_keys('\n')
 
 		self.check_for_row_in_list_table('1: Buy milk')
 		error = self.browser.find_element_by_css_selector('.has-error')
 		self.assertEqual(error.text, "You can't have an empty list item")
 
-		self.get_item_input_box(self).send_keys('Make tea\n')
+		self.get_item_input_box().send_keys('Make tea\n')
 		self.check_for_row_in_list_table('1: Buy milk')
 		self.check_for_row_in_list_table('2: Make tea')
-
-		self.fail('Finish the test!')
